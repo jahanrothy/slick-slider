c43ca96818c3fdd9a519e9455bc4e710a9998c07
jahanrothy <jahanrothy58@gmail.com>
2024-05-23 19:23:31 +0600

    slick slider complete

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+    
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:'<span class="priv_arrow"><i class="ri-arrow-left-s-line"></i></span>',
+        nextArrow:'<span class="next_arrow"><i class="ri-arrow-right-s-line"></i></span>',
+      });
+
+});
